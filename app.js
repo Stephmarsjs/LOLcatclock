8cbaf51e7515f9b59ab4fd272b3e325a091a051b
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-08-19 09:51:13 -0400

    added clock and drop down

---
+++ app.js
@@ -0,0 +1,13 @@
+var clock = document.getElementById('displayClock');
+setInterval(function(){
+  clock.innerHTML = getCurrentTime();
+}, 1);
+function getCurrentTime() {
+  var currentDate = new Date();
+  var hours = currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
+  hours === 0 ? hours = 12 : hours = hours;
+  var minutes = currentDate.getMinutes();
+  var seconds = currentDate.getSeconds() < 10 ? '0' + currentDate.getSeconds() : currentDate.getSeconds();
+  var currentTime = hours + ':' + minutes + ':' + seconds;
+  return currentTime;
+}
